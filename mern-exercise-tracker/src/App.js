54c13ecc078131efad8bba958ba20579a59cab44
NsL01 <lahdoninos76@gmail.com>
2019-10-24 11:11:59 +0100

    Learn MERN

---
+++ mern-exercise-tracker/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Navbar from "./components/navbar.component.js";
+import ExercisesList from './components/exercises-list.component.js';
+import EditExercise from './components/edit-exercise.component.js';
+import CreateExercise from './components/create-exercise.component.js';
+import CreateUser from './components/create-user.component.js';
+
+function App() {
+    return(
+        <Router>
+            <div className="container">
+                <Navbar />
+                <br />
+                <Route path="/" exact component={ExercisesList} />
+                <Route path="/edit/:id" exact component={EditExercise} />
+                <Route path="/create" exact component={CreateExercise} />
+                <Route path="/user" exact component={CreateUser} />
+            </div>
+        </Router>
+    )
+}
+
+export default App;
